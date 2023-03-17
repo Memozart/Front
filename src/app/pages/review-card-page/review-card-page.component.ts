1ac7b3149603093c01dfa11e38f0b2ad05942e1a
julieep <julie.emerard@sfr.fr>
2023-03-17 19:23:19 +0100

    style: 💄 start design for review page

--- src/app/pages/review-card-page/review-card-page.component.ts
+++ src/app/pages/review-card-page/review-card-page.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-review-card-page',
   templateUrl: './review-card-page.component.html',
-  styleUrls: ['./review-card-page.component.css']
+  styleUrls: ['./review-card-page.component.css'],
 })
 export class ReviewCardPageComponent {
+  value: number = 20;
 
+  constructor() {}
 }
