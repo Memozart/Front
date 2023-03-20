4f0d4bf349444bfd74c6843db843b02b0cd9e346
julieep <julie.emerard@sfr.fr>
2023-03-20 22:20:43 +0100

    feat: 🎸 call route to get oldest review in theme "langue"

--- src/app/pages/review-card-page/review-card-page.component.ts
+++ src/app/pages/review-card-page/review-card-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpService } from './../../services/http.service';
 
 @Component({
   selector: 'app-review-card-page',
@@ -7,6 +8,16 @@ import { Component } from '@angular/core';
 })
 export class ReviewCardPageComponent {
   value: number = 20;
+  review!: any;
 
-  constructor() {}
+  constructor(private http: HttpService) {}
+
+  ngOnInit() {
+    this.http.get('reviews/640b15c689e35929e7675db2').subscribe({
+      next: (res: any) => {
+        this.review = res.body.review;
+        console.log(this.review);
+      },
+    });
+  }
 }
