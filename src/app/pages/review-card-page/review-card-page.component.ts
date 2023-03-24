a8c1fb708dc2a3a74617298b24515ba8805f75f4
julieep <julie.emerard@sfr.fr>
2023-03-24 18:01:00 +0100

    feat: 🎸 call route to submit a review

--- src/app/pages/review-card-page/review-card-page.component.ts
+++ src/app/pages/review-card-page/review-card-page.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { HttpService } from './../../services/http.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ResponseService } from 'src/app/services/response.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-review-card-page',
@@ -9,14 +12,47 @@ import { HttpService } from './../../services/http.service';
 export class ReviewCardPageComponent {
   value: number = 20;
   review!: any;
+  reviewForm!: FormGroup;
 
-  constructor(private http: HttpService) {}
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpService,
+    private response: ResponseService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
-    this.http.get('reviews/640b15c689e35929e7675db2').subscribe({
+    this.getReviewByTheme('640b15c689e35929e7675db2');
+
+    this.reviewForm = this.fb.group({
+      answer: ['', [Validators.required]],
+      idReview: ['', [Validators.required]],
+    });
+  }
+
+  getReviewByTheme = (theme_id: any) => {
+    this.http.get('reviews/' + theme_id).subscribe({
       next: (res: any) => {
         this.review = res.body.review;
         console.log(this.review);
+      },
+    });
+  };
+
+  submitReview() {
+    this.reviewForm.controls['idReview'].setValue(this.review._id);
+    const answerData = this.reviewForm.value;
+    console.log(answerData);
+
+    if (this.reviewForm.invalid) return;
+
+    this.http.post('reviews', answerData).subscribe({
+      next: (res: any) => {
+        console.log(res);
+        // display modal response
+      },
+      error: (err: any) => {
+        this.response.errorF(err, 'Erreur');
       },
     });
   }
