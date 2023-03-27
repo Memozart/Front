4200c01844d75074a3cbb40ed8de55c5c58eceec
Antonin BRINES <antonin.brines@outlook.fr>
2023-03-27 12:07:53 +0200

    feat: 🎸 refresh toekn

--- src/app/interceptors/bearer.interceptor.ts
+++ src/app/interceptors/bearer.interceptor.ts
@@ -3,18 +3,79 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpErrorResponse,
+  HttpClient,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError, switchMap, finalize } from 'rxjs/operators';
+import { HttpService } from '../services/http.service';
 
 @Injectable()
 export class BearerInterceptor implements HttpInterceptor {
+  private isRefreshing = false;
+  constructor(private httpService: HttpService) {}
 
-  constructor() {}
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<any> {
+    request = this.addToken(request);
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    const authToken = localStorage.getItem('token');
-    const authReq = request.clone({ setHeaders: { Authorization: `Bearer ${authToken}` } });
-    return next.handle(authReq);
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 401) {
+          if (this.isRefreshing) {
+            return throwError(error);
+          }
+
+          this.isRefreshing = true;
+
+          return this.refreshToken().pipe(
+            switchMap(() => {
+              request = this.addToken(request);
+              return next.handle(request);
+            }),
+            catchError((err) => {
+              return throwError(error);
+            }),
+            finalize(() => {
+              this.isRefreshing = false;
+            })
+          );
+        }
+        return throwError(error);
+      })
+    );
+  }
+
+  private addToken(request: HttpRequest<any>): HttpRequest<any> {
+    const token = localStorage.getItem('access_token');
+    if (token) {
+      return request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    }
+    return request;
+  }
+
+  private refreshToken(): Observable<any> {
+    const refreshToken = localStorage.getItem('refresh_token');
+
+    return new Observable((observer) => {
+      this.httpService
+        .post('refresh-token', {
+          refresh_token: refreshToken,
+        })
+        .pipe(
+          catchError((error: HttpErrorResponse) => {
+            return throwError(error);
+          })
+        )
+        .subscribe((response: any) => {
+          localStorage.setItem('access_token', response.accessToken);
+          observer.next();
+          observer.complete();
+        });
+    });
   }
 }
