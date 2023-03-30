580ff2515f5b382d99d0f4f59803ac951200a777
julieep <julie.emerard@sfr.fr>
2023-03-30 16:50:55 +0200

    refactor: 💡 card component evolution with ng content

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -8,26 +8,4 @@ import { Card } from './../../models/card';
 })
 export class CardComponent {
   @Input() card!: Card;
-  @Input() openConfirmDialog!: () => void;
-  boxShadow!: string;
-  hoverEdit: boolean = false;
-  hoverDel: boolean = false;
-
-  func(bool: boolean) {
-    return `background: ${this.card.theme.darkColor};
-    border: 0;
-    boxShadow: ${
-      bool
-        ? 'inset 3px 3px 4px ' +
-          this.card.theme.darkShadow +
-          ', inset -2px -2px 4px ' +
-          this.card.theme.lightShadow +
-          ''
-        : '2px 2px 4px ' +
-          this.card.theme.darkShadow +
-          ', -1px -1px 4px ' +
-          this.card.theme.lightShadow +
-          ''
-    } `;
-  }
 }
