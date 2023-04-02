13f4e841998f47c0280af9cea9072c407eb97de7
julieep <julie.emerard@sfr.fr>
2023-04-02 15:44:38 +0200

    feat: 🎸 improve modals design on manage card page

--- src/app/pages/manage-card-page/manage-card-page.component.ts
+++ src/app/pages/manage-card-page/manage-card-page.component.ts
@@ -1,11 +1,15 @@
-import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import {
+  FormGroup,
+  FormBuilder,
+  FormControl,
+  Validators,
+} from '@angular/forms';
 import { Card } from './../../models/card';
 import { HttpService } from './../../services/http.service';
 import { Component, OnInit } from '@angular/core';
 import { ResponseService } from 'src/app/services/response.service';
 import { Theme } from 'src/app/models/theme';
 import { DesignService } from 'src/app/services/design.service';
-
 
 @Component({
   selector: 'app-manage-card-page',
@@ -14,29 +18,35 @@ import { DesignService } from 'src/app/services/design.service';
 })
 export class ManageCardPageComponent implements OnInit {
   cards: Card[] = [];
-  themes : Theme[] = [];  
+  themes: Theme[] = [];
   selectedCard?: Card;
   showUpdateDialog = false;
   showdeleteDialog = false;
   updateCardFormGroup!: FormGroup;
+  theme!: Theme;
+  bgLinearGradient!: string;
+  selectedTheme!: string;
 
-  
   constructor(
     private http: HttpService,
     private fb: FormBuilder,
     private response: ResponseService,
     public designService: DesignService
-  ) { }
+  ) {}
+
+  ngOnDestroy() {
+    this.designService.resetCustomBgColor();
+  }
 
   ngOnInit() {
     this.updateCardFormGroup = this.fb.group({
       question: new FormControl('', [Validators.required]),
       answer: new FormControl('', [Validators.required]),
       theme: new FormControl('', [Validators.required]),
-      help: new FormControl('')
+      help: new FormControl(''),
     });
 
-    this.http.get("themes").subscribe({
+    this.http.get('themes').subscribe({
       next: (res: any) => {
         this.themes = res.body as Theme[];
       },
@@ -55,35 +65,40 @@ export class ManageCardPageComponent implements OnInit {
     });
   }
 
-  showDialog(card: Card, isModalUpdate : boolean = true){
+  showDialog(card: Card, isModalUpdate: boolean = true) {
     this.selectedCard = card;
-    if(!isModalUpdate){ // c'est la modal de suppression
-      this.selectedCard = card;
+    this.getSelectedTheme(this.selectedCard.theme._id);
+    if (!isModalUpdate) {
+      this.showUpdateDialog = false;
       this.showdeleteDialog = true;
       return;
     }
 
+    this.showdeleteDialog = false;
     this.showUpdateDialog = true;
     this.updateCardFormGroup.setValue({
-      question: card.question, 
+      question: card.question,
       answer: card.answer,
       theme: card.theme._id,
-      help: card.help
+      help: card.help,
     });
   }
 
   updateCard() {
-    if(this.updateCardFormGroup.invalid || this.selectedCard === null){
+    if (this.updateCardFormGroup.invalid || this.selectedCard === null) {
       this.showUpdateDialog = false;
+      this.designService.resetCustomBgColor();
       return;
     }
 
     const cardData = this.updateCardFormGroup.value;
-    this.http.update("cards",this.selectedCard!._id, cardData).subscribe({
+    this.http.update('cards', this.selectedCard!._id, cardData).subscribe({
       next: (res: any) => {
-        const foundIndex = this.cards.findIndex(x => x._id === this.selectedCard!._id);
+        const foundIndex = this.cards.findIndex(
+          (x) => x._id === this.selectedCard!._id
+        );
         this.cards[foundIndex] = res.body;
-        this.closeModal()
+        this.closeModal();
         this.response.successF(
           'Mise à jour effectuée',
           'La carte a bien été mise à jour'
@@ -94,18 +109,20 @@ export class ManageCardPageComponent implements OnInit {
         this.closeModal();
         this.response.errorF(
           err,
-          'Une erreur à eut lieu pendant la mise à jour de la carte'
+          'Une erreur a eu lieu pendant la mise à jour de la carte'
         );
-      }
-    })
+      },
+    });
   }
 
   deleteCard() {
-    if (!this.selectedCard)
-      return;
+    if (!this.selectedCard) return;
+
     this.http.delete('cards', this.selectedCard._id).subscribe({
       next: (res: any) => {
-        this.cards = this.cards.filter(card => card._id !== this.selectedCard?._id);
+        this.cards = this.cards.filter(
+          (card) => card._id !== this.selectedCard?._id
+        );
         this.response.successF(
           'Suppression effectuée',
           'La carte a bien été suprimée'
@@ -119,22 +136,8 @@ export class ManageCardPageComponent implements OnInit {
           'Une erreur à eut lieu pendant la suppression de la carte'
         );
         this.closeModal();
-      }
-    })
-  }
-
-  changeBtnStyle(e: any, card: Card, hovered: boolean = false) {
-    e.currentTarget.style.boxShadow = hovered
-      ? 'inset 3px 3px 4px ' +
-      card.theme.darkShadow +
-      ', inset -2px -2px 4px ' +
-      card.theme.lightShadow +
-      ''
-      : '2px 2px 4px ' +
-      card.theme.darkShadow +
-      ', -1px -1px 4px ' +
-      card.theme.lightShadow +
-      '';
+      },
+    });
   }
 
   /**
@@ -144,5 +147,21 @@ export class ManageCardPageComponent implements OnInit {
     this.selectedCard = undefined;
     this.showUpdateDialog = false;
     this.showdeleteDialog = false;
+    this.designService.resetCustomBgColor();
+  }
+
+  getSelectedTheme(id: string) {
+    this.http.get('themes/' + id).subscribe({
+      next: (data: any) => {
+        this.theme = data.body;
+        this.bgLinearGradient =
+          'linear-gradient(' +
+          this.theme.color1 +
+          ', ' +
+          this.theme.color2 +
+          ')';
+        this.designService.changeCustomBgColor(this.bgLinearGradient);
+      },
+    });
   }
 }
