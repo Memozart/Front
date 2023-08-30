81af83840a08c3dd7e1514e23698cba7591f4434
julieep <julie.emerard@sfr.fr>
2023-08-30 22:17:13 +0200

    refactor: 💡 amélioration composant input

--- src/app/shared/input-text/input-text.component.ts
+++ src/app/shared/input-text/input-text.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component, Input, Optional, Self } from '@angular/core';
+import { FormControl, FormGroup, FormGroupDirective } from '@angular/forms';
 
 @Component({
   selector: 'app-input-text',
@@ -8,10 +8,15 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class InputTextComponent {
   @Input() inputId!: string;
+  @Input() type: string = 'text';
+  @Input() label!: string;
   @Input() formGroup!: FormGroup;
-  @Input() type!: string;
 
   getFormControl(): FormControl {
     return this.formGroup.get(this.inputId) as FormControl;
+  }
+
+  isInvalidInput(formGroup: FormGroup, name: string): boolean {
+    return !!formGroup.get(name)?.invalid && formGroup.get(name)!.touched;
   }
 }
